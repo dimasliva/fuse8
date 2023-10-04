6612b43768d6964b037a9b1d8db02987ec9e00e2
dimasliva <janzerikYT@yandex.ru>
2023-10-04 12:23:10 +0500

    deploy

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,25 +1,8 @@
 import { type User } from "@/types/User";
-import { type Events } from "@/types/Events";
 import { defineStore } from 'pinia'
 
 export type UserState = {
   user: User,
-  events: {
-    "id": Number,
-    "name": String,
-    "address": String,
-    "date": String,
-    "status": String,
-    "comment": String
-}[],
-event: {
-  "id": Number,
-  "name": String,
-  "address": String,
-  "date": String,
-  "status": String,
-  "comment": String
-}
 };
 
 export const useUserStore = defineStore({
