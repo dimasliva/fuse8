e9e3d8be06d3bd0b3e108f06ce85b78a66c75117
dimasliva <janzerikYT@yandex.ru>
2023-10-04 16:22:37 +0500

    form local dev

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -16,9 +16,10 @@ export const useUserStore = defineStore({
         name: '',
         role: ''
       },
-      url: 'https://my-json-server.typicode.com/dimasliva/fuse8_json_server/'
+      // url если нужно запустить на gh-pages
+      // url: 'https://my-json-server.typicode.com/dimasliva/fuse8_json_server/'
       // url если нужно запустить локально
-      // url: window.location.protocol + '//' + window.location.hostname + ':3001/'
+      url: window.location.protocol + '//' + window.location.hostname + ':3001/'
     } as UserState),
     actions: {
       setUser(user: User) {
